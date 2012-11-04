6b40ca2ee31fccfb590cdfb6e35d0f27aa38a5a7
tuxfrickler <mendress@tuxfrickler.de>
2012-11-04 22:15:10 +0100

    0.2 release --> threading, nice output boxes and fixed some stuff

--- ePetCrawler.py
+++ ePetCrawler.py
@@ -1,131 +1,225 @@
 __author___ = 'Marco Endress'
-__version__ = '0.1'
-__date__ = '11/01/2012'
+__version__ = '0.2'
+__date__    = '11/04/2012'
+__progname__= 'ePetGrabber'
 
-import sys, os
+import sys, os, time
 import re
 import mechanize
+import Queue
 from BeautifulSoup import BeautifulSoup
-
+import threading
+"""
 epetitionAccount = { 
         'u'  : 'yourUsername',
         'pw' : 'yourPassword'
     }
+    """
 
+########################################################################
 class Args:
-    """
-    command line argumentsn
-    """
+    """ command line arguments """
+    #----------------------------------------------------------------------
     def __init__(self, usage='Usage: %(progname)s [opt...] [file...]'):
         "init, usage string: embed program name as %(progname)s"
         self.progname = os.path.basename(sys.argv[0])
         self._argv = sys.argv[1:]
         self._usage = usage
         
+    #----------------------------------------------------------------------
     def usage(self):
-        "print usage message, and exit"
+        """ print usage message, and exit """
         print >> sys.stderr
         print >> sys.stderr, self._usage % {'progname': self.progname}
         print >> sys.stderr        
         sys.exit(1)
-
-class EpetitionsCrawler:
-    """
-    grab epetition data 
-    """
-    
-    def __init__(self, petId):
-        "init"
-        self._petId = petId
-        self.br = None
-        self.host = 'https://epetitionen.bundestag.de'
-        self.grabUrl = 'mitzeichnungsliste.$$$.a.u.page.\
-                        @.batchsize.100.html#pagerbottom'
-        self.pageCounter = 0
-        self.subscrCount = 0
         
+########################################################################
+class SideDownloader(threading.Thread):
+    """ Threaded Side Downloader """
+ 
+    #----------------------------------------------------------------------
+    def __init__(self, queue, cookie):
+        """ init Side Downloader """
+        threading.Thread.__init__(self)
+        self.queue = queue
+        self.threadBrowser = mechanize.Browser()
+        self.threadBrowser._ua_handlers['_cookies'].cookiejar = cookie
+ 
+    #----------------------------------------------------------------------
+    def run(self):
+        """"""
+        while True:
+            url = self.queue.get()
+            self.download_side(url)
+            self.queue.task_done()
+ 
+    #----------------------------------------------------------------------
+    def download_side(self, url):
+        """"""
+        try:
+            side = self.threadBrowser.open(url)
+            html = side.read()
+            fname = os.path.basename(url)
+            with open(fname, "wb") as f:
+                f.write(html)
+            
+        except Exception, ex:
+            print ex
+            
+########################################################################
+class EpetitionsGrabber:
+    """ grab epetition data """
+    #----------------------------------------------------------------------
+    def __init__(self, petId):
+        """ init EpetitionsGrabber """
+        self._petId = petId
+        self._br = None
+        self._host = 'https://epetitionen.bundestag.de'
+        self._grabUrl = 'mitzeichnungsliste.$$$.a.u.page.@.batchsize.100.html#pagerbottom'
+        
+    #----------------------------------------------------------------------
     def grabpetID(self):
-        ""
-        print "+++++++++++++++++++++++++++++++++++++"
-        print "+\tEpetitionsCrawler %s       +" % (__version__)
-        print "+++++++++++++++++++++++++++++++++++++"
+        """"""
+        self.printMsg(__progname__ + " " + __version__, 1)
         self.openBr()
-        self.logIn()
-        self.br.select_form(name="suchform")
-        self.br.form['suchbegriff'] = self._petId
-        r = self.br.submit()
-        lastLink = list(self.br.links(text_regex=re.compile(self._petId)))[-1]
-        html = self.br.open(lastLink.url)
+        self.login()
+        self._br.select_form(name="suchform")
+        self._br.form['suchbegriff'] = self._petId
+        r = self._br.submit()
+        lastLink = list(self._br.links(text_regex=re.compile(self._petId)))[-1]
+        self._grabUrl = lastLink.url.split('$$$')[0] + self._grabUrl
+        html = self._br.open(lastLink.url)
         pattern = r'(\d+)*\n&'
         matches = re.compile(pattern).findall(html.read())
         
         if matches == []:
-            self.subscrCount = -1
+            subscrCount = -1
+            #raise
         else:
-            self.subscrCount = matches[0]
-            
-        self.pageCounter = int(self.subscrCount) / 100
+            subscrCount = matches[0]
+            pageCounter = self.calcPages(subscrCount)
         
-        if (int(self.subscrCount) % 100) > 0:
-            self.pageCounter += 1
-            
-        print '%s subscribers(s) -> %i pages to parse' % (self.subscrCount, self.pageCounter)
-        s = lastLink.url.split('$$$')[0]
-        f = open(self._petId, 'a')
+        self.printMsg("Petition %s:\n%s subscribers(s)\n-> %i sides to parsing " % (self._petId, subscrCount , pageCounter), 0)
+        start_timer = time.time()
+        
+        queue = Queue.Queue()
+        fileList = []
+        cookie = self._br._ua_handlers['_cookies'].cookiejar
+ 
+        # create a thread pool and give them a queue
+        for i in xrange(15):
+            threadLocal = SideDownloader(queue, cookie)
+            threadLocal.setDaemon(True)
+            threadLocal.start()
 
-        for i in range(0, self.pageCounter + 1):
-            page = self.host + s + self.grabUrl.replace('@', str(i))
-            html = self.br.open(page)
-            soup = BeautifulSoup(''.join(html))
-            table = soup.find('table', id="subscriber")
-            f.write(str(table))
-            
+        for i in xrange(0, pageCounter):
+            url = self._grabUrl.replace('@', str(i))
+            queue.put(self._host + url)
+            fileList.append(url)
+       
+        queue.join()
+        self.writeOut(fileList)
+        latency = time.time() - start_timer
+        self.printMsg("time left: %s" % str(latency), 0)
+        self.printMsg("all done ;-)", 2)
         self.closeBr()
-    
+    #----------------------------------------------------------------------
     def openBr(self):
-        "open mechanize session"
-        self.br = mechanize.Browser()
-        self.br.addheaders = [
+        """ open mechanize session """
+        self._br = mechanize.Browser()
+        self._br.addheaders = [
         ( 'User-agent', 
           'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) \
               Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-    
+              
+    #----------------------------------------------------------------------
     def closeBr(self):
-        "close mechanize session"
-        self.br.close()
-        
-    def logIn(self):
-        "login to epetitionen.bundestag.de with username and password"
+        """ close mechanize session """
+        self._br.close()
+    
+    #----------------------------------------------------------------------
+    def login(self):
+        """ login to epetitionen.bundestag.de with username and password """
         try:
-            self.br.open("https://epetitionen.bundestag.de/epet/anmelden.html")
-            self.br.select_form(nr=1)
-            self.br.form['j_username'] = epetitionAccount['u']
-            self.br.form['j_password'] = epetitionAccount['pw']
-            html = self.br.submit()
+            self._br.open("https://epetitionen.bundestag.de/epet/anmelden.html")
+            self._br.select_form(nr=1)
+            self._br.form['j_username'] = epetitionAccount['u']
+            self._br.form['j_password'] = epetitionAccount['pw']
+            html = self._br.submit()
             pattern = 'posteingang'
             matches = re.compile(pattern).findall(html.read())
            
             if matches == []:
-                raise Exception('login failure!\n\tcheck username and\n\tpassword')
+                raise Exception('login failure!\ncheck username and\npassword')
             else:
-                print "login okay"
+                self.printMsg("login okay", 0)
             
         except Exception, ex:
-            print "\tError: %s" % ex
+            #print "\tError: %s" % ex
+            self.printMsg(str(ex), 0)
             sys.exit(1)
-
-      #def searchpetID(self):
-
+      
+    #----------------------------------------------------------------------
+    def calcPages(self, subscribers):
+        """ calculate number of sides to parsing """
+        pageCounter = int(subscribers) / 100
+        
+        if (int(subscribers) % 100) > 0:
+            pageCounter += 1
+            
+        return pageCounter
+        
+    #----------------------------------------------------------------------
+    def writeOut(self, fileList):
+        """ parse html files and write output file """
+        table = ''
+        for item in fileList:
+            fhandler = open(os.path.basename(item), "r")
+            raw_html = fhandler.read()
+            soup = BeautifulSoup(''.join(raw_html))
+            table += str(soup.find('table', id="subscriber"))
+      
+        filename = self._petId + '.html'
+        with open(filename, 'w') as f:
+            f.write(table)
+    
+    #----------------------------------------------------------------------
+    def printMsg(self, msg, outBorder):
+        """ print nice output """
+        try:
+            border = "+++++++++++++++++++++++++++++++++++++++"
+            if outBorder == 1:
+                print border
+            
+            if len(msg) <= len(border):
+                borderRight = "+"
+                borderRight = borderRight.rjust(len(border) - len(msg) - 2)
+                print "+ %s"  % (msg + borderRight)
+            else:
+                newlines = msg.count('\n')
+                for i in xrange(newlines + 1):
+                      borderRight = "+"
+                      borderRight = borderRight.rjust(len(border) - len(msg.split('\n')[i]) - 2)
+                      print "+ %s"  % (msg.split('\n')[i] + borderRight)
+                
+            if outBorder == 2:
+                print border
+            else:
+                print "---------------------------------------"
+                
+        except Exception, ex:
+            print ex
+#--------------------------------------------------------------------------
 def main():
-    """
-    """
+    """"""
     a = Args('Usage: %(progname)s [-p petitionId] [-f filename]')
     "ToDo clean up"
     if len(sys.argv) != 3:
         a.usage()
         
-    petCrawler = EpetitionsCrawler(sys.argv[2])
-    petCrawler.grabpetID()
+    petGrabber = EpetitionsGrabber(sys.argv[2])
+    petGrabber.grabpetID()
 
 if __name__ == "__main__":
     main()
